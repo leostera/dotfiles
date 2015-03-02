d8015766bce8f6ff16c2f3ec051487613c33a2c4
Leandro Ostera <me@leostera.com>
2015-03-02 02:02:12 -0300

    adds python, ruby, and alias for pip.
    also link python apps and setup pip

--- install/brew.sh
+++ install/brew.sh
@@ -11,10 +11,15 @@ brew install git git-extras
 brew install gnu-sed --default-names
 brew install grep --default-names
 brew install npm
-brew install python3
+brew install python python3
+brew install ruby
 brew install sbcl 			# Steel Bank Common Lisp
 brew install tldr
 brew install tmux
 brew install tree
 brew install vim
 brew install wget
+
+brew linkapps python
+pip install --upgrade setuptools
+pip install --upgrade pip
