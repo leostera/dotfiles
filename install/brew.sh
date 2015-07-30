479edeabefca0ba4a95d5707f951c813e0f8ba4e
Leandro Ostera <leandro@ostera.io>
2015-07-30 11:35:20 +0200

    Adds nmap

--- install/brew.sh
+++ install/brew.sh
@@ -9,20 +9,21 @@ brew install aircrack-ng
 brew install asciinema
 brew install coreutils
 brew install dockutil
+brew install erlang rebar
 brew install git git-extras
 brew install gnu-sed --default-names
 brew install grep --default-names
+brew install nmap
 brew install npm
 brew install python python3
 brew install ruby
 brew install sbcl 			# Steel Bank Common Lisp
-brew install erlang rebar
 brew install tldr
 brew install tmux
 brew install tree
 brew install vim
-brew install wget
 brew install visionmedia-watch
+brew install wget
 
 pip install --upgrade setuptools
 pip install --upgrade pip
