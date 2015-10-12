3e8e25579bf86b6bc5587a9dc9ddccb1e002d893
Leandro Ostera <leandro@ostera.io>
2015-10-12 19:57:56 +0200

    Only install if uname is darwin

--- install.sh
+++ install.sh
@@ -11,17 +11,18 @@ done
 
 mkdir -p $HOME/.vim
 ln -sfv "`pwd`/vim/vundle.vim" $HOME/.vim
-symlink vim/.vimrc
 
 # Install time!
 source ./install/prepare.sh
 source ./install/zsh.sh
-source ./install/brew.sh
 source ./install/npm.sh
 source ./install/vundle.sh
 
 # OS X 
-#source ./install/brew-cask.sh
-#source ./osx/defaults.sh
+if [[ `uname` == "Darwin" ]]; then
+  source ./install/brew.sh
+  source ./install/brew-cask.sh
+  source ./osx/defaults.sh
+fi
 
 source $HOME/.zshrc
