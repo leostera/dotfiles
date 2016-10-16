d83d4271b1db9affb78b68ffcd0b4d4ac1425fe0
Leandro Ostera <leandro@ostera.io>
2016-10-16 16:32:21 +0200

    readable script

--- tmux/scripts/find.sh
+++ tmux/scripts/find.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-readonly SELECTION=$( tmux list-window -a -F "#{session_name}:#{window_index} #{window_name} #{pane_current_path}" | column -t  | fzf )
+readonly SELECTION=$(
+  tmux list-window -a \
+    -F "#{session_name}:#{window_index} #{window_name} #{pane_current_path}" \
+  | column -t \
+  | fzf
+)
 
 readonly TMUX_SELECTOR=$( echo $SELECTION | awk '{ print $1 }' )
 readonly TMUX_SESSION=$( echo $TMUX_SELECTOR | awk -F':' '{ print $1 }')
