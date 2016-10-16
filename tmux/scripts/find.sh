b87eb52ee40a9fe1800907f0e5feca39841abdf5
Leandro Ostera <leandro@ostera.io>
2016-10-16 15:43:23 +0200

    Close #8. Fuzzy find!

---
+++ tmux/scripts/find.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+readonly SELECTION=$( tmux list-window -a -F "#{session_name}:#{window_index} #{window_name} #{pane_current_path}" | column -t  | fzf )
+
+readonly TMUX_SELECTOR=$( echo $SELECTION | awk '{ print $1 }' )
+readonly TMUX_SESSION=$( echo $TMUX_SELECTOR | awk -F':' '{ print $1 }')
+readonly TMUX_WINDOW=$( echo $TMUX_SELECTOR | awk -F':' '{ print $2 }')
+
+tmux switch -t $TMUX_SESSION
+tmux select-window -t $TMUX_WINDOW
