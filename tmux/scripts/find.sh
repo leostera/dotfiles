10a98318dc8fc4da62839ae73779fa75ef8af757
Leandro Ostera <leandro@ostera.io>
2016-10-26 22:09:28 +0200

    Ultimate just-take-me-there tmux flow

--- tmux/scripts/find.sh
+++ tmux/scripts/find.sh
@@ -1,15 +1,31 @@
-#!/bin/bash
+#!/bin/zsh
 
-readonly SELECTION=$(
+# For some reason readonly _eats_ the last exit status
+SEARCH_RESULTS=$(
   tmux list-window -a \
     -F "#{session_name}:#{window_index} #{window_name} #{pane_current_command} #{pane_current_path}" \
   | column -t \
-  | fzf
+  | fzf --print-query
 )
+readonly MATCHED=$?
+readonly QUERY=$( echo $SEARCH_RESULTS | cut -d " " -f 1 )
+readonly SELECTION=$( echo $SEARCH_RESULTS | cut -d " " -f 1- )
 
-readonly TMUX_SELECTOR=$( echo $SELECTION | awk '{ print $1 }' )
-readonly TMUX_SESSION=$( echo $TMUX_SELECTOR | awk -F':' '{ print $1 }')
-readonly TMUX_WINDOW=$( echo $TMUX_SELECTOR | awk -F':' '{ print $2 }')
+# If we haven't matched anything
+if [[ $MATCHED -eq 1 ]]; then
+  readonly DIR=$(cat ~/.z | cut -d'|' -f 1 | fzf --query=$SELECTION\$ -1 -0)
+  # And this is one of our recent directories
+  if [[ ! -z $DIR ]]; then
+    # Well let's go in there already!
+    tmux new-window -a -n $SELECTION -c $DIR
+  fi
+# If we have matched on an existing window
+elif [[ $MATCHED -eq 0 ]]; then
+  readonly TMUX_SELECTOR=$( echo $SELECTION | awk '{ print $1 }' )
+  readonly TMUX_SESSION=$( echo $TMUX_SELECTOR | awk -F':' '{ print $1 }')
+  readonly TMUX_WINDOW=$( echo $TMUX_SELECTOR | awk -F':' '{ print $2 }')
 
-tmux switch -t $TMUX_SESSION
-tmux select-window -t $TMUX_WINDOW
+  # Well let's go in there already!
+  tmux switch -t $TMUX_SESSION
+  tmux select-window -t $TMUX_WINDOW
+fi
