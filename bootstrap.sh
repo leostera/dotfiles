fd377bb31640970c08beebb26865759013d55af7
Leandro Ostera <leandro@ostera.io>
2017-05-15 22:36:00 +0200

    Split symlinking step on it's own file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-function symlink {
-    ln -sfv "`pwd`/$1" $HOME
-}
-
-# Symlinking all dotfiles!
-for dotfile in */.*; do
-  symlink $dotfile;
-done
-
-ln -sfv `pwd`/vim $HOME/.vim
-ln -sfv `pwd`/tmux $HOME/.tmux
-
 # Install time!
+source ./install/symlink.sh
 source ./install/prepare.sh
 source ./install/zsh.sh
 source ./install/vundle.sh
---
+++ install/symlink.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+function symlink {
+  ln -sfv "`pwd`/$1" $HOME
+}
+
+# Symlinking all dotfiles!
+for dotfile in */.*; do
+  symlink $dotfile;
+done
+
+ln -sfv `pwd`/vim $HOME/.vim
+ln -sfv `pwd`/tmux $HOME/.tmux
