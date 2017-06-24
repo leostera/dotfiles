82290c47b822bf5bb409a6a365adfa1e239d2187
Leandro Ostera <leandro@ostera.io>
2017-06-24 14:30:17 +0200

    Set muffintosh as the default name

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -2,15 +2,18 @@
 
 sudo -v
 
-sudo scutil --set ComputerName "muffintosh"
-sudo scutil --set HostName "muffintosh"
-sudo scutil --set LocalHostName "muffintosh"
+readonly HOSTNAME="muffintosh"
+
+sudo scutil --set ComputerName ${HOSTNAME}
+sudo scutil --set HostName ${HOSTNAME}
+sudo scutil --set LocalHostName ${HOSTNAME}
+sudo hostname ${HOSTNAME}
 
 # Disable audio feedback when volume is changed
 defaults write com.apple.sound.beep.feedback -bool false
 
 # Show battery percentage
-defaults write com.apple.menuextra.battery -bool true
+defaults write com.apple.menuextra.battery ShowPercent NO
 
 # Disable the "Are you sure oyu want to open this application?" dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
@@ -27,6 +30,7 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Trackpad: enable tap to click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
