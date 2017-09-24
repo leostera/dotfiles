f5117cfaebcd6a494280e939cacce8bfd74c774f
Leandro Ostera <leandro@ostera.io>
2017-09-24 14:13:17 +0200

    Refactor reorganize repos to include remotes

--- git/reorganize_repos.sh
+++ git/reorganize_repos.sh
@@ -2,18 +2,26 @@
 
 readonly REPOS=$(ls ~/repos)
 
-function printProject {
+function moveProject {
   local NAME=$1
   local DIR=~/repos/${1}
   local IS_REPO=$DIR/.git
   if [[ -d ${DIR} && -d ${IS_REPO} ]]; then
-    local PROJECT=$(cd ${DIR} && git remote -v | head -n 1 | awk -F'/' '{print $4}');
-    echo "Moving ${NAME} into ${PROJECT} -> ~/repos/${PROJECT}/${NAME}";
-    mkdir -p ~/repos/${PROJECT};
-    mv ${DIR} ~/repos/${PROJECT};
+    local PROJECT=$(
+      cd ${DIR} && git remote -v \
+        | head -n 1 \
+        | awk -F'/' '{print $4}' );
+    local REMOTE=$(
+      cd ${DIR} && git remote -v \
+        | head -n 1 \
+        | awk -F'/' '{print $1}' \
+        | awk -F'@' '{print $2}' );
+    echo "Moving ${NAME} into ${REMOTE}/${PROJECT} -> ~/repos/${REMOTE}/${PROJECT}/${NAME}";
+    mkdir -p ~/repos/${REMOTE}/${PROJECT};
+    mv ${DIR} ~/repos/${REMOTE}/${PROJECT};
   fi
 }
 
 for dir in ${REPOS}; do
-  printProject ${dir}
+  moveProject ${dir}
 done
