21155270e48f2717065c1dac6c151b5bf0c59658
Leandro Ostera <leandro@ostera.io>
2019-03-01 09:39:37 +0100

    Fix curling archive

--- remote-install.sh
+++ remote-install.sh
@@ -7,7 +7,7 @@ mkdir -p ${DOTFILES}
 cd ${DOTFILES}
 
 echo "Downloading dotfiles..."
-curl https://github.com/ostera/dotfiles/archive/master.tar.gz
+curl https://codeload.github.com/ostera/dotfiles/tar.gz/master > master.tar.gz
 
 echo "Extracting..."
 tar -xf -C .
