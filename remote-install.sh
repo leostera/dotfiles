9dc9807694014c4101bdf2136710dc487bda8c6f
Leandro Ostera <leandro@ostera.io>
2019-03-01 09:44:37 +0100

    Can't depend on GNU tar either

--- remote-install.sh
+++ remote-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DOTFILES=~/repos/github.com/ostera/dotfiles
+DOTFILES=~/repos/github.com/ostera
 
 echo "Creating dotfiles path..."
 mkdir -p ${DOTFILES}
@@ -10,7 +10,11 @@ echo "Downloading dotfiles..."
 curl https://codeload.github.com/ostera/dotfiles/tar.gz/master > master.tar.gz
 
 echo "Extracting..."
-tar -xf -C .
+tar -xf master.tar.gz
 rm master.tar.gz
+
+mv dotfiles-master dotfiles
+
+cd dotfiles
 
 ./bootstrap.sh
