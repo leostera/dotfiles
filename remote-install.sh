9f3f80e5d37860bf33bed11ac5dfbac1d2807796
Leandro Ostera <leandro@ostera.io>
2019-03-01 09:37:18 +0100

    Don't depend on git for first install

--- remote-install.sh
+++ remote-install.sh
@@ -2,6 +2,15 @@
 
 DOTFILES=~/repos/github.com/ostera/dotfiles
 
-git clone https://github.com/ostera/dotfiles ${DOTFILES}
+echo "Creating dotfiles path..."
+mkdir -p ${DOTFILES}
 cd ${DOTFILES}
-[ -f "bootstrap.sh" ] && ./bootstrap.sh
+
+echo "Downloading dotfiles..."
+curl https://github.com/ostera/dotfiles/archive/master.tar.gz
+
+echo "Extracting..."
+tar -xf -C .
+rm master.tar.gz
+
+./bootstrap.sh
