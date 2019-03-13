6189ff6007c310aede95ab07f70227bb6fda3ce9
Leandro Ostera <leandro@ostera.io>
2019-03-13 14:01:44 +0100

    Make email configurable for ssh keygen step

--- misc/ssh-keygen.sh
+++ misc/ssh-keygen.sh
@@ -2,10 +2,14 @@
 
 ID_RSA=~/.ssh/id_rsa
 
+if [ -z ${EMAIL} ]; then
+  EMAIL=leandro@ostera.io
+fi
+
 if [ ! -f ${ID_RSA} ]; then
   echo "Missing default ssh keys at ${ID_RSA}!"
   echo "Creating pair..."
-  ssh-keygen -t rsa -b 4096 -C "leandro@ostera.io"
+  ssh-keygen -t rsa -b 4096 -C "${EMAIL}"
   echo "Done:"
 else
   echo "Using existing ssh keys found at ${ID_RSA}:"
