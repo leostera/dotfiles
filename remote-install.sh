77694d79815f91f2f44f6c2bb9507284c35ce180
Leandro Ostera <leandro@ostera.io>
2019-03-13 14:02:33 +0100

    Update remote-install.sh

--- remote-install.sh
+++ remote-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DOTFILES=~/repos/github.com/ostera
+DOTFILES=~/repos/github.com/ostera/dotfiles
 
 echo "Creating dotfiles path..."
 mkdir -p ${DOTFILES}
@@ -10,11 +10,6 @@ echo "Downloading dotfiles..."
 curl https://codeload.github.com/ostera/dotfiles/tar.gz/master > master.tar.gz
 
 echo "Extracting..."
-tar -xf master.tar.gz
-rm master.tar.gz
-
-mv dotfiles-master dotfiles
-
-cd dotfiles
+tar -xf -C .
 
 ./bootstrap.sh
