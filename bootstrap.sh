180d16805cda682ee63d45a0effb36da00ba0ba7
Leandro Ostera <leandro@abstractmachines.dev>
2021-12-02 22:17:46 +0100

    fix: configure channels

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
+if [[ ! `which nix-env` ]]; then
+  sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
 
 if [[ `uname` == "Darwin" ]]; then
   nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
   ./result/bin/darwin-installer
   rm -rf ./result
 fi
+
+sudo nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+sudo nix-channel --update
+
+exec zsh
 
 ./tools/setup-upstream
 ./tools/symlink
--- home/.config/nixpkgs/darwin/configuration.nix
+++ home/.config/nixpkgs/darwin/configuration.nix
@@ -1,16 +1,18 @@
 { config, pkgs, ...}:
 
 {
+  imports = [
+    ../home.nix
+  ];
+
   environment.systemPackages =
     with pkgs; [
       chromiumWrapper
       firefoxWrapper
     ];
 
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToControl = true;
-  };
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
 
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
