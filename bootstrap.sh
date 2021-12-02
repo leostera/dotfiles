5bf4b5e0747c7c7f1a43b91dd3c50f0bbdb968b6
Leandro Ostera <leandro@abstractmachines.dev>
2021-12-02 21:52:20 +0100

    feat: setup upstream before symlinking

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
+
+if [[ `uname` == "Darwin" ]]; then
+  nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+  ./result/bin/darwin-installer
+  rm -rf ./result
+fi
+
+./tools/setup-upstream
 ./tools/symlink
