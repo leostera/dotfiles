30b364afbcb82b73209891811efdd9fe8d7653af
Leandro Ostera <leandro@abstractmachines.dev>
2025-11-28 16:43:37 +0100

    update to eza and ditch nix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-if [[ ! `which nix-env` ]]; then
-  sh <(curl -L https://nixos.org/nix/install) --daemon
-fi
-
 if [[ ! `which brew` ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
@@ -12,12 +8,5 @@ fi
 
 echo "Starting new shell..."
 exec zsh
-
-sudo nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
-sudo nix-channel --update
-
-if [[ `uname` == "Darwin" ]]; then
-  ./tools/setup-nix-darwin
-fi
 
 ./tools/setup-upstream
